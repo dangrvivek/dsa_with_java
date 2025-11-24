6675ead99dfa1fcbfddc2cecf9e23bf61094636b
vivek dangar <vivekdangar179@gmail.com>
2025-11-24 23:20:59 +0530

    added ptn1

--- patterns/Ptn1.java
+++ patterns/Ptn1.java
@@ -2,6 +2,11 @@ package dsa_with_java.patterns;
 
 public class Ptn1 {
     public static void main(String args[]) {
-        System.out.println("hello word");
+        for(int i=0; i<=5; i++) {
+            for(int j=0; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
